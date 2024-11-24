73f989b69e3428d839a878cf72ef58a0f3385b01
Mustaphabns <med.mustapha.benslimane@gmail.com>
2024-11-24 10:47:24 +0100

    :tada: commencer le projet

---
+++ src/main/java/com/exercie/api/actionnaire/ActionnaireApplication.java
@@ -0,0 +1,13 @@
+package com.exercie.api.actionnaire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActionnaireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActionnaireApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exercie/api/actionnaire/ActionnaireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exercie.api.actionnaire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActionnaireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
